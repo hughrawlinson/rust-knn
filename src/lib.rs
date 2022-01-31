146bc9a84773d615ae95436f3acd690560eb2bc6
Hugh Rawlinson <hughr2005@gmail.com>
2022-01-31 15:02:26 +0100

    Move logic into lib

---
+++ src/lib.rs
@@ -0,0 +1,39 @@
+mod datum;
+mod point;
+mod point2d;
+
+pub use datum::Datum;
+pub use point::Point;
+pub use point2d::Point2d;
+
+pub trait Distance {
+  fn distance(&self, other: &Self) -> f64;
+}
+
+pub trait NewRandom {
+  fn new_random() -> Self;
+}
+
+pub fn create_dataset<T: Distance + NewRandom>(number: i32) -> Vec<Datum<T>> {
+  let mut dataset = Vec::new();
+  for _i in 0..number {
+    dataset.push(Datum::new_random());
+  }
+  dataset
+}
+
+pub fn k_nearest_neighbors<'a, T: Distance>(
+  k: usize,
+  query_point: &T,
+  dataset: &'a Vec<Datum<T>>,
+) -> Vec<(&'a Datum<T>, f64)> {
+  let mut dataset_with_distances: Vec<(&Datum<T>, f64)> = dataset
+    .into_iter()
+    .map(|datum| (datum, datum.distance_from_point(&query_point)))
+    .collect();
+
+  dataset_with_distances
+    .sort_by(|(_, distance_a), (_, distance_b)| distance_a.partial_cmp(distance_b).unwrap());
+
+  dataset_with_distances.into_iter().take(k).collect()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,7 @@
 use clap::{ArgEnum, Parser};
 use serde::Serialize;
 
-mod datum;
-mod point;
-mod point2d;
-
-use crate::datum::Datum;
-use crate::point::Point;
-use crate::point2d::Point2d;
-
-pub trait Distance {
-  fn distance(&self, other: &Self) -> f64;
-}
-
-pub trait NewRandom {
-  fn new_random() -> Self;
-}
-
-fn create_dataset<T: Distance + NewRandom>(number: i32) -> Vec<Datum<T>> {
-  let mut dataset = Vec::new();
-  for _i in 0..number {
-    dataset.push(Datum::new_random());
-  }
-  dataset
-}
-
-fn k_nearest_neighbors<'a, T: Distance>(
-  k: usize,
-  query_point: &T,
-  dataset: &'a Vec<Datum<T>>,
-) -> Vec<(&'a Datum<T>, f64)> {
-  let mut dataset_with_distances: Vec<(&Datum<T>, f64)> = dataset
-    .into_iter()
-    .map(|datum| (datum, datum.distance_from_point(&query_point)))
-    .collect();
-
-  dataset_with_distances
-    .sort_by(|(_, distance_a), (_, distance_b)| distance_a.partial_cmp(distance_b).unwrap());
-
-  dataset_with_distances.into_iter().take(k).collect()
-}
+use nearest_neighbor::*;
 
 #[derive(ArgEnum, Clone, Debug)]
 enum PointType {
